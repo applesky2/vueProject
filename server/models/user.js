c6e92d72e3fee46f569edfc94a5c7e003519191f
applesky2 <3021984098@qq.com>
2021-07-24 18:42:42 +0800

    登录接口及登录框

--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,9 @@
 var mongoose = require('mongoose');
 var userSchema = new mongoose.Schema({
-    "userId": String,
-    "userName": String,
-    "userPwd": String,
-    "orderList": Array,
+    "userId":String,
+    "userName":String,
+    "userPwd":String,
+    "orderList":Array,
     "cartList": [
         {
             "productId": String,
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,38 +1,135 @@
 <template>
   <div class="header">
-      <div class="header-content">
-        <div class="left-header">
-          <i
-            class="el-icon-platform-eleme"
-            style="font-size: 50px; color: #409eff"
-          ></i
-          ><span class="logo-title">商城</span>
-        </div>
-        <div class="right-header">
-          <span></span>
-          <span class="login-header">Login</span>
-          <span
-            ><i class="el-icon-shopping-cart-2" style="font-size: 50px"></i
-          ></span>
-        </div>
+    <div class="header-content">
+      <div class="left-header">
+        <i
+          class="el-icon-platform-eleme"
+          style="font-size: 50px; color: #409eff"
+        ></i
+        ><span class="logo-title">商城</span>
+      </div>
+      <div class="right-header">
+        <span></span>
+        <span class="login-header" @click="login()">Login</span>
+        <span
+          ><i class="el-icon-shopping-cart-2" style="font-size: 50px"></i
+        ></span>
       </div>
     </div>
+    <el-dialog
+      title="Login"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :before-close="handleClose"
+    >
+      <el-form
+        :model="ruleForm"
+        status-icon
+        :rules="rules"
+        ref="ruleForm"
+        label-width="60px"
+        class="demo-ruleForm"
+      >
+        <el-form-item label="用户名" prop="userName">
+          <el-input v-model="ruleForm.userName"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="pass">
+          <el-input
+            type="password"
+            v-model="ruleForm.pass"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('ruleForm')"
+            >登录</el-button
+          >
+          <el-button @click="resetForm('ruleForm')">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-  components:{},
-  props:{},
-  data(){
+  components: {},
+  props: {},
+  data() {
+    var checkUserName = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error("用户名不能为空"));
+      }
+      setTimeout(() => {
+        if (value.length > 64) {
+          callback(new Error("最大支持64位字符"));
+        } else {
+          callback();
+        }
+      }, 1000);
+    };
+    var validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入密码"));
+      } else {
+        callback();
+      }
+    };
     return {
-    }
+      dialogVisible: false,
+      ruleForm: {
+        pass: "",
+        userName: "",
+      },
+      rules: {
+        pass: [{ validator: validatePass, trigger: "blur" }],
+        userName: [{ validator: checkUserName, trigger: "blur" }],
+      },
+    };
   },
-  watch:{},
-  computed:{},
-  methods:{},
-  created(){},
-  mounted(){}
-}
+  watch: {},
+  computed: {},
+  methods: {
+    handleClose(done) {
+      done();
+    },
+    login() {
+      this.dialogVisible = true;
+    },
+    submit(){
+      axios.post("/users/login", {
+        userName: this.ruleForm.userName,
+        userPwd: this.ruleForm.pass,
+      }).then((response)=>{
+        const res = response.data;
+        this.dialogVisible = false;
+        if (res.status === '0') {
+          this.$message({
+          message: '登录成功',
+          type: 'success'
+        });
+        } else {
+          this.$message.error(res.message);
+        }
+      })
+    },
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.submit();
+        } else {
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+  },
+  created() {},
+  mounted() {},
+};
 </script>
 <style scoped>
 .header {
@@ -66,6 +163,7 @@ export default {
 .login-header {
   font-size: 26px;
   font-weight: 600;
+  cursor: pointer;
 }
 </style>
 
